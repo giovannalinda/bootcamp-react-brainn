80e40e4a1e10766d79032a59359f894a115f8700
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-08-31 17:10:45 -0300

    Destructuring Assignment

---
+++ aulas-complementares/destructuring-assignment/main.js
@@ -0,0 +1,10 @@
+const person = {
+  identification: 'Giovanna',
+  age: 18,
+}
+
+// const identification = person.identification
+const { identification } = person
+console.log('o nome é', identification)
+const age = person.age
+console.log('a idade é', age)
