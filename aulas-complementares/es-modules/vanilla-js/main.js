402cb58622e345701a8071be4fc87f3ff3017a73
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-08-31 14:45:14 -0300

    ES Modules

---
+++ aulas-complementares/es-modules/vanilla-js/main.js
@@ -0,0 +1,6 @@
+// * as exporta tudo de um arquivo
+import teste, { sum, mult } from './calculator.js'
+
+console.log('sum:', sum(1, 2))
+console.log('mult:', mult(1, 2))
+console.log('teste:', teste)
