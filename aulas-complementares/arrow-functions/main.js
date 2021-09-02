2f20ea69b7a7769f17c066ddd42c6ca2f83ffb16
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-09-01 23:54:04 -0300

    arrow functions

--- aulas-complementares/arrow-functions/main.js
+++ aulas-complementares/arrow-functions/main.js
@@ -1,3 +1,37 @@
-function sum(a, b) {
+/*
+  function sum(a, b) {
+    return a + b
+  }
+
+  const sum = function(a, b) {
+    return a + b
+}*/
+
+const sum = (a, b) => {
   return a + b
 }
+
+console.log('sum:', sum(1,2))
+// quando tem somente return
+const mult = (a, b) => a * b
+
+console.log('mult:', mult(2,2))
+// não precisa de parenteses com um argumento
+const plusOne = a => a + 1
+
+console.log('plusOne:', plusOne(5))
+
+const getReturn = () => {
+  return {
+    name: 'Giovanna'
+  }
+}
+
+console.log('getReturn:', getReturn())
+
+// sintaxe retornando objetos
+const getObject = () => ({
+	name: 'Giovanna'
+})
+
+console.log('getObject:', getObject())
