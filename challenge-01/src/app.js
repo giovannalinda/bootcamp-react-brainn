23951ca61ef45c969ab87dfe5a90b586bd595b3f
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-09-02 16:06:29 -0300

    footer

--- challenge-01/src/app.js
+++ challenge-01/src/app.js
@@ -2,6 +2,7 @@ import Header from './header'
 import Menu from './menu'
 import Sidebar from './sidebar'
 import Content from './content'
+import Footer from './footer'
 
 function App() {
   return ( 
@@ -10,6 +11,7 @@ function App() {
       <Menu />,
       <Sidebar />,
       <Content />,
+      <Footer />
     </div>
   )
 }
---
+++ challenge-01/src/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+      <p>&copy; Brainn.co Servicos de Tecnologia LTDA</p>
+    </div>
+  )
+}
