35b96e00f3199400496e4d50988d8880543cc622
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-09-03 19:19:48 -0300

    array

--- challenge-02/src/app.js
+++ challenge-02/src/app.js
@@ -1,8 +1,48 @@
+import { useState } from 'react';
+
 import Header from './header'
 import Menu from './menu'
 import Sidebar from './sidebar'
 import Content from './content'
 import Footer from './footer'
+
+const links = [
+  { 
+    id: 1,
+    title: "O que faz um desenvolvedor Front end",
+    content: (
+    <>
+      <h1>O que faz um desenvolvedor Front end</h1>
+      <p>O desenvolvedor front-end é responsável pela experiência do usuário dentro de uma aplicação web, é ele quem vai desenhar e desenvolver as páginas com as quais, posteriormente, o usuário irá interagir. </p>
+      <h2>Como aprender?</h2>
+      <p>Para ser um desenvolvedor front-end você precisa se familiarizar com lógica de programação aprender algumas linguagens, como por exemplo: </p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Frameworks</li>
+      </ul>
+    </>
+      )
+    },
+    {
+      id: 2,
+      title: "O que o react pode fazer",
+      content: (
+        <>
+          <h1>O que o React pode fazer</h1>
+          <p>O React é uma biblioteca JavaScript para construir interfaces de usuário</p>
+          <h4>O que você vai aprender</h4>
+          <ul>
+            <li>Destructuring Assignment</li>
+            <li>JSX</li>
+            <li>Composição de componentes</li>
+            <li>Componentes customizados</li>
+          </ul>
+        </>
+      )
+    }
+  ]
 
 function App() {
   return ( 
@@ -11,7 +51,7 @@ function App() {
       <Menu />,
       <Sidebar />,
       <Content />,
-      <Footer />
+      <Footer />,
     </div>
   )
 }
--- challenge-02/src/sidebar.js
+++ challenge-02/src/sidebar.js
@@ -1,18 +1,7 @@
-import H3 from './h3'
-import H1 from './h1'
-import Button from './button'
-
 export default function Sidebar() {
   return (
     <aside className="sidebar">
-      <H1>Bootcamp React</H1>
-      <H3>
-        O bootcamp é um programa prático desenvolvido para melhorar suas habilidades e conhecimentos técnicos e te ajudar a ingressar no mercado de trabalho.
-      </H3>
-      <p>
-        O Bootcamp React acontece ao longo de 4 semanas e é voltado para aqueles que querem se desenvolver em React.js. Você aprenderá com grandes profissionais do mercado a aprofundar seu conhecimento técnico, aprimorar suas skills e atingir seu potencial de carreira.
-      </p>
-      <Button kind="primary">Inscrever-se</Button>
+     
     </aside>
   )
 }
