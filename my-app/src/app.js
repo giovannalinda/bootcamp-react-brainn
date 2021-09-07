ed4657e1bb241c499a627eb20914c26e103d3d03
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-09-07 18:38:38 -0300

    useEffect

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,36 +1,31 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
+
+const url = 'https://ws.apicep.com/cep.json?code=[CEP]'
 
 function App() {
-  const [inputValue, setInputValue] = useState('')
-  console.log('input (estado): ', inputValue)
-  
-  const obj = {
-    a: 1
-  }
-
-  const obj2 = {
-    ...obj,
-    b: 2,
-    c: 3,
-    d: 4
-  }
-
-  const { c, ...obj3 } = obj2
-
-  console.log('obj', obj)
-  console.log('obj2', obj2)
-  console.log('obj3', obj3)
-
-  function handleChange(event) {
-    console.log('input value', event.target.value)
-    setInputValue(event.target.value)
-  }
-
+  console.log('montando componente...')
+  useEffect(() => {
+    async function getCep() {
+      console.log('buscando cep...')
+      const response = await fetch(url.replace('[CEP]', '08344620'))
+      const json = await response.json()
+      console.log('cep', json)
+    }
+    getCep()
+  })
+  /*
+    async function handleClick() {
+    console.log('buscando cep...')
+    const response = await fetch(url.replace('[CEP]', '08344620'))
+    const json = await response.json()
+    console.log('cep', json)
+    }
+    console.log('2) componente pronto para montar')
+  */
   return (
-    <form>
-      <input value={inputValue} onChange={handleChange}/>
-      <button type="submit">Enviar</button>
-    </form>
+    <>
+      <button>Buscar CEP</button>
+    </>
   )
 }
 
