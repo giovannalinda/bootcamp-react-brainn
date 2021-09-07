670c6971c40776722d39b378b97dd8e7bc09d627
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-09-07 01:31:44 -0300

    forms

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,37 +1,20 @@
 import { useState } from 'react'
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  /*
-  const state = useState(0)
-  const counter = state[0]
-  const setCounter = state[1]
-  */
-
-  function increment() {
-    setCounter(counter + 1)
-  }
-
-  function decrement() {
-    setCounter(counter - 1)
+  const [inputValue, setInputValue] = useState('')
+  console.log('input (estado): ', inputValue)
+  
+  function handleChange(event) {
+    console.log('input value', event.target.value)
+    setInputValue(event.target.value)
   }
 
   return (
-    <>
-      {counter === null 
-      ? <h1>Contador zerado</h1> 
-      : <h1>Contador: {counter}</h1>
-      }
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-    </>
+    <form>
+      <input value={inputValue} onChange={handleChange}/>
+      <button type="submit">Enviar</button>
+    </form>
   )
 }
 
 export default App
-/*
-const button = document.querySelector('button')
-button.addEventListener('click', (event) => {
-
-})
-*/
