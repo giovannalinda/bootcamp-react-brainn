162218fc021f74dac86a4d2202979f13561de123
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-09-09 01:30:46 -0300

    challenge-03 complete

--- challenge-03/src/App.js
+++ challenge-03/src/App.js
@@ -27,11 +27,11 @@ function App() {
     httpPost(url, car)
       .then((response) => {
         if (response.error) {
-          alert(response.message) 
-        } else {
-          alert('Carro cadastrado com sucesso!')
-          setCars([...cars, car])
-        }
+          return alert(response.message)
+        } 
+
+        alert('Carro cadastrado com sucesso!')
+        setCars([...cars, car])
       })
   
     event.target.reset()
@@ -41,10 +41,9 @@ function App() {
     httpDelete(url, {plate})
       .then((response) => {
         if (response.error) {
-          alert(response.message)
-        } else {
-          setCars(cars.filter(car => car.plate !== plate))
-        }
+          return alert(response.message)
+        } 
+        setCars(cars.filter(car => car.plate !== plate))
       }
     )
   }
